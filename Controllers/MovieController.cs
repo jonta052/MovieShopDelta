e44b545cfa5ad73c21f9252ab44f74817a9d1470
Zara Fahlström <zara.fahlstrom@gmail.com>
2022-08-19 15:16:53 +0200

    Added function and view to add a movie to the database.

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -1,18 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MovieShopDelta.Models.Database;
+using MovieShopDelta.Data;
+
 
 namespace MovieShopDelta.Controllers
 {
     public class MovieController : Controller
     {
+        AppDbContext db = new AppDbContext();
+
         // GET: Movie
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult AddMovie()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddMovie([Bind(Include = "Id,Title,Director,ReleaseYear,Genre,Price")] Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Movies.Add(movie);
+                db.SaveChanges();
+                return RedirectToAction("AddMovie");
+            }
+
+            return View(movie);
         }
     }
 }
