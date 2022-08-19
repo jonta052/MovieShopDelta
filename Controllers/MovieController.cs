002e5e6a30af1fbd645b4ee178d23248b913187f
Andre <arocha.work@gmail.com>
2022-08-19 14:09:40 +0200

    Page that shows all Movies

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -1,18 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data.Entity;
 using System.Web;
 using System.Web.Mvc;
 using MovieShopDelta.Models.Database;
+using MovieShopDelta.Data;
 
 namespace MovieShopDelta.Controllers
 {
     public class MovieController : Controller
     {
+        AppDbContext db = new AppDbContext();
         // GET: Movie
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult AllMovies()
+        {
+            var movieList = db.Movies.ToList();
+            return PartialView("_AllMovies",movieList);
         }
     }
 }
