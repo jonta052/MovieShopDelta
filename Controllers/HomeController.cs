55ddfd7942b19808913db2a8bb90c3a162b04c09
Zara Fahlström <zara.fahlstrom@gmail.com>
2022-08-23 12:27:09 +0200

    Added query to display the customer with the most expensive order.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MovieShopDelta.Models.Database;
+using MovieShopDelta.Models.ViewModels;
 using MovieShopDelta.Models;
 using MovieShopDelta.Data;
 
@@ -38,19 +39,25 @@ namespace MovieShopDelta.Controllers
                 .Join(db.OrderRows,
                 m => m.Id,
                 o => o.MovieId,
-                (m, o) => new {
+                (m, o) => new 
+                {
                     MovieId = o.MovieId,
                     Title = m.Title
                 })
+                
                 .GroupBy(movie => movie.MovieId)
+                
                 .Select(obj => new MostPopularMoviesVM
                 {
                     MovieId = obj.Key,
                     Count = obj.Count(),
                     Title = obj.FirstOrDefault().Title,
                 })
+                
                 .OrderByDescending(count => count.Count)
+                
                 .Take(5)
+                
                 .ToList();
 
             return PartialView(mostPopularMovies);
@@ -58,54 +65,91 @@ namespace MovieShopDelta.Controllers
 
         public ActionResult FiveNewestMovies()
         {
-            var fiveNewestMovies = (from movie in db.Movies
-                                    orderby movie.ReleaseYear descending
-                                    select movie)
-                                   .Take(5)
-                                   .ToList();
-                                   
-            //var fiveNewestMovies = db.Movies
-            //    .OrderByDescending(y => y.ReleaseYear)
-            //    .Take(5)
-            //    .ToList();
+            //var fiveNewestMovies = (from movie in db.Movies
+            //                        orderby movie.ReleaseYear descending
+            //                        select movie)
+            //                       .Take(5)
+            //                       .ToList();
+
+            var fiveNewestMovies = db.Movies
+                .OrderByDescending(y => y.ReleaseYear)
+                .Take(5)
+                .ToList();
 
             return PartialView("_FiveNewestMovies",fiveNewestMovies);
         }
 
         public ActionResult FiveOldestMovies()
         {
-            var fiveOldestMovies = (from movie in db.Movies
-                                    orderby movie.ReleaseYear
-                                    select movie)
-                                   .Take(5)
-                                   .ToList();
+            //var fiveOldestMovies = (from movie in db.Movies
+            //                        orderby movie.ReleaseYear
+            //                        select movie)
+            //                       .Take(5)
+            //                       .ToList();
 
-            //var fiveOldestMovies = db.Movies
-            //    .OrderBy(y => y.ReleaseYear)
-            //    .Take(5)
-            //    .ToList();
+            var fiveOldestMovies = db.Movies
+                .OrderBy(y => y.ReleaseYear)
+                .Take(5)
+                .ToList();
 
             return PartialView("_FiveOldestMovies",fiveOldestMovies);
         }
 
         public ActionResult FiveCheapestMovies()
         {
-            var fiveCheapestMovies = (from movie in db.Movies
-                                    orderby movie.Price
-                                    select movie)
-                       .Take(5)
-                       .ToList();
-            //var fiveCheapestMovies = db.Movies
-            //    .OrderBy(p => p.Price)
-            //    .Take(5)
-            //    .ToList();
+            //var fiveCheapestMovies = (from movie in db.Movies
+            //                        orderby movie.Price
+            //                        select movie)
+            //           .Take(5)
+            //           .ToList();
+
+            var fiveCheapestMovies = db.Movies
+                .OrderBy(p => p.Price)
+                .Take(5)
+                .ToList();
 
             return PartialView("_FiveCheapestMovies",fiveCheapestMovies);
         }
 
         public ActionResult MostExpensiveOrder()
         {
-            return PartialView();
+            var mostExpensiveOrder = db.Orders
+                .Join(db.OrderRows,
+                ord => ord.Id, 
+                orderrow => orderrow.OrderId,
+                (ord1, orderrow) => new 
+                {
+                    OrderId = ord1.Id,
+                    Price = orderrow.Price,
+                    CustomerId = ord1.CustomerId
+                })
+                
+                .Join(db.Customers,
+                ord2 => ord2.CustomerId,
+                cust => cust.Id,
+                (ord2, cust) => new 
+                {
+                    OrderId = ord2.OrderId,
+                    Price = ord2.Price,
+                    CustomerId = ord2.CustomerId,
+                    FirstName = cust.FirstName,
+                    LastName = cust.LastName
+                })
+                
+                .GroupBy(custid => custid.CustomerId)
+                
+                .Select(obj => new MostExpensiveOrderVM
+                {
+                    FirstName = obj.FirstOrDefault().FirstName,
+                    LastName = obj.FirstOrDefault().LastName,
+                    SumOrder = obj.Sum(sum => sum.Price)
+                })
+                
+                .OrderByDescending(sum => sum.SumOrder)
+                
+                .FirstOrDefault();
+
+            return PartialView(mostExpensiveOrder);
         }
 
     }
