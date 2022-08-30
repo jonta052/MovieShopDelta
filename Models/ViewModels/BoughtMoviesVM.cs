cfa7ca7849178ae8dc430ef15d1c625bfdf3231c
jonta052 <55479519+jonta052@users.noreply.github.com>
2022-08-30 09:04:16 +0200

    Buttons for shopping cart added

---
+++ Models/ViewModels/BoughtMoviesVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieShopDelta.Models.ViewModels
+{
+    public class BoughtMoviesVM
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        [DataType(DataType.Currency)]
+        public Decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
