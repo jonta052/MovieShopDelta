b7dd437deff1bd0b8bfac44ec82929f9ff8a9229
Andre <arocha.work@gmail.com>
2022-09-02 13:52:31 +0200

    changes

--- Migrations/202209021131347_CreateDB.Designer.cs
+++ Migrations/202209021131347_CreateDB.Designer.cs
@@ -7,13 +7,13 @@ namespace MovieShopDelta.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.4.4")]
-    public sealed partial class initial : IMigrationMetadata
+    public sealed partial class CreateDB : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(CreateDB));
         
         string IMigrationMetadata.Id
         {
-            get { return "202209020901042_initial"; }
+            get { return "202209021131347_CreateDB"; }
         }
         
         string IMigrationMetadata.Source
